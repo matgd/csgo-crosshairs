eeae815814a7a0635b7b23e3f4b14d04fe9f0d93
matgd <matgd@users.noreply.github.com>
2020-07-21 17:19:50 +0200

    Move crosshair codes outside index.html

---
+++ crosshair_codes.js
@@ -0,0 +1,12 @@
+const crosshairs = [
+  'CSGO-RCKzo-oGuRa-J5FzV-YvtMx-36SKF',
+  'CSGO-5EXfH-v2rYW-AGYm2-MYqFq-TNqNO',
+  'CSGO-YhxuK-CkYhp-2S38o-5VtMC-xpjjD',
+  'CSGO-TxoYv-k6mta-xEWm8-3mFqm-6dMwF',
+  'CSGO-WW9Nt-X5tSs-2bJ95-Rc3z6-EDCcO',
+  'CSGO-d5ZbO-ASr2H-HEnCq-QHJY3-qXJzN',
+  'CSGO-LEo9E-vxAOA-5Opib-3aacz-WULoD',
+  'CSGO-mKC3t-mqH3F-PhsO5-vymtv-uFFZG',
+  'CSGO-wQW7s-3csTp-QSqQ7-sJZQF-LqbxJ',
+  'CSGO-VmaUc-kqUBo-hq3ME-aqYqw-EvPcF',
+];
